40e0de338705802d8dc31495074c767bbce3964b
Martin Akesson <c0rner@users.noreply.github.com>
2014-05-02 20:52:47 +0200

    Added agentfix and colors scripts to dotfiles/inc

---
+++ inc/agentfix.sh
@@ -0,0 +1,52 @@
+# Agentfix - Keep SSH_AUTH_SOCK working in multi-sessions; https://github.com/c0rner/dotfiles
+
+#  Multiple ssh sessions combined  with a terminal multiplexer will completely
+#  mess up SSH_AUTH_SOCK. This script tries to set things straight..
+
+# Do not run outside TMUX or SCREEN
+[ -z "${TMUX}" -a -z "${STY}" ] && return
+
+# Unset SSH_AUTH_SOCK if set and invalid
+if [ -n "${SSH_AUTH_SOCK}" ]; then
+   if [ ! -S "${SSH_AUTH_SOCK}" ]; then
+      unset SSH_AUTH_SOCK
+   else
+      ssh-add 2>/dev/null
+      if [ $? -eq 2 ]; then
+         rm -f "${SSH_AUTH_SOCK}"
+         unset SSH_AUTH_SOCK
+      fi
+   fi
+fi
+
+# SSH_AUTH_SOCK not set, try finding a socket
+if [ -z "${SSH_AUTH_SOCK}" ]; then
+   if [ -S "${HOME}/.ssh/agent" ]; then
+      SSH_AUTH_SOCK="${HOME}/.ssh/agent"
+   else
+      # Search for sockets owned by us in /tmp
+      for dir in $(ls -d "/tmp/ssh-"*); do
+         if [ ${UID} -eq $(stat "${dir}" -c "%u") ]; then
+            SSH_AUTH_SOCK=$(readlink -f "${dir}/agent"*)
+            if [ -S "${SSH_AUTH_SOCK}" ]; then
+               ssh-add 2>/dev/null
+               [ $? -lt 2] && break
+            fi
+            unset SSH_AUTH_SOCK
+         fi
+      done
+   fi
+fi
+
+# Create static link to a working socket
+if [ ! -S "${HOME}/.ssh/agent" ]; then
+   [ -L "${HOME}/.ssh/agent" ] && rm "${HOME}/.ssh/agent"
+   if [ -n "${SSH_AUTH_SOCK}" ]; then
+      ln -s "${SSH_AUTH_SOCK}" "${HOME}/.ssh/agent"
+      SSH_AUTH_SOCK="${HOME}/.ssh/agent"
+      export SSH_AUTH_SOCK
+   fi
+else
+   SSH_AUTH_SOCK="${HOME}/.ssh/agent"
+   export SSH_AUTH_SOCK
+fi
---
+++ inc/colors.sh
@@ -0,0 +1,28 @@
+[ -n $BASH_VERSINFO -a $BASH_VERSINFO -eq 4 ] || { echo "error: need BASH v4"; return; }
+
+# TermInfo feature detection (Borrowed from LiquidPrompt)
+if tput setaf >/dev/null 2>&1 ; then
+   ti_setaf () { tput setaf "$1" ; }
+elif tput AF >/dev/null 2>&1 ; then
+   # *BSD
+   ti_setaf () { tput AF "$1" ; }
+elif tput AF 1 >/dev/null 2>&1 ; then
+   # OpenBSD
+   ti_setaf () { tput AF "$1" ; }
+else
+   echo "error: terminal '$TERM' not supported" >&2
+   ti_setaf () { : ; }
+fi
+
+declare -A HYAC
+HYAC["BOLD"]="$( { tput bold || tput md ; } 2>/dev/null )"
+HYAC["BLACK"]="$(ti_setaf 0)"
+HYAC["RED"]="$(ti_setaf 1)"
+HYAC["GREEN"]="$(ti_setaf 2)"
+HYAC["YELLOW"]="$(ti_setaf 3)"
+HYAC["BLUE"]="$(ti_setaf 4)"
+HYAC["MAGENTA"]="$(ti_setaf 5)"
+HYAC["CYAN"]="$(ti_setaf 6)"
+HYAC["WHITE"]="$(ti_setaf 7)"
+HYAC["RESET"]="$( { tput sgr0 || tput me ; } 2>/dev/null )"
+unset ti_setaf
